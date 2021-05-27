e9a89825769c492e52222b1ad2b0f57931f6ee17
Dhyon Keyllo <dhyon.kpm@hotmail.com>
2021-05-27 09:03:28 -0300

    Iniciando usuarios
    iniciando com o layout de usuarios

---
+++ application/controllers/Usuarios.php
@@ -0,0 +1,27 @@
+<?php
+
+	defined('BASEPATH') or exit('Ação não permitida');
+
+	class Usuarios extends CI_Controller {
+		
+		public function __construct() {
+			parent::__construct();
+			
+
+			// definir se há sessão
+		}
+
+		public function index() {
+
+			$data = array(
+				'usuarios' =>  $this->ion_auth->users()->result() // recupera todos os usuarios
+			);
+
+			$this->load->view('layout/header', $data);
+			$this->load->view('usuarios/index');
+			$this->load->view('layout/footer');
+
+			
+		}
+		
+	}
