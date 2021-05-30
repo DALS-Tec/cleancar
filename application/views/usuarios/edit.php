be52c4744d5e3a7be869dd272f0a6acea0cea708
Dhyon Keyllo <dhyon.kpm@hotmail.com>
2021-05-30 14:21:41 -0300

    Update edit.php

--- application/views/usuarios/edit.php
+++ application/views/usuarios/edit.php
@@ -72,15 +72,23 @@
 								
 							</div>
 
-							<div class="form-group">
-								<label for="exampleInputPassword1">Password</label>
-								<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+							<div class="form-group row">
+
+								<div class="col-md-6">
+									<label>Senha</label>
+									<input type="password" class="form-control" name="password" aria-describedby="emailHelp" placeholder="Sua senha">
+									<small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+								</div>
+
+								<div class="col-md-6">
+									<label>Confirme</label>
+									<input type="password" class="form-control" name="confirm_password" aria-describedby="emailHelp" placeholder="Confirme sua senha">
+									<small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+								</div>
+								
 							</div>
-							<div class="form-check">
-								<input type="checkbox" class="form-check-input" id="exampleCheck1">
-								<label class="form-check-label" for="exampleCheck1">Check me out</label>
-							</div>
-							<button type="submit" class="btn btn-primary">Submit</button>
+							
+							<button type="submit" class="btn btn-primary btn-sm">Salvar</button>
 							</form>
                         </div>
 
