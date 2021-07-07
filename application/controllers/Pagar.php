5a0ef36fa36e52a872344c41fd48d64fbef24315
Dhyon Keyllo <dhyon.kpm@hotmail.com>
2021-07-07 10:01:41 -0300

    feat(view): Pagar

--- application/controllers/Pagar.php
+++ application/controllers/Pagar.php
@@ -44,6 +44,71 @@ class Pagar extends CI_Controller{
     }
 
 	public function edit($conta_pagar_id = NULL) {
+        if (!$conta_pagar_id || !$this->core_model->get_by_id('contas_pagar', array('conta_pagar_id' => $conta_pagar_id))) {
+            $this->session->set_flashdata('error', 'Conta não encontrada');
+            redirect('pagar');
+        } else {
+
+            $this->form_validation->set_rules('conta_pagar_fornecedor_id', '', 'required');
+            $this->form_validation->set_rules('conta_pagar_data_vencimento', '', 'required');
+            $this->form_validation->set_rules('conta_pagar_valor', '', 'required');
+            $this->form_validation->set_rules('conta_pagar_obs', 'Observações', 'max_length[100]');
+
+            if ($this->form_validation->run()) {
+                $data = elements(
+                    array(
+                            'conta_pagar_fornecedor_id',
+                            'conta_pagar_data_vencimento',
+                            'conta_pagar_valor',
+                            'conta_pagar_status',
+                            'conta_pagar_obs',
+                        ),
+                    $this->input->post()
+                );
+                    
+
+                $conta_pagar_status = $this->input->post('conta_pagar_status');
+
+                if ($conta_pagar_status == 1) {
+                    $data['conta_pagar_data_pagamento'] = date('Y-m-d H:i:s');
+                }
+                    
+                $data = html_escape($data);
+
+                $this->core_model->update('contas_pagar', $data, array('conta_pagar_id' => $conta_pagar_id));
+
+                redirect('pagar');
+            } else {
+
+                    //erro de validacao
+
+                $data = array(
+
+                        'titulo' => 'Pagar cadastrados',
+            
+                        'styles' => array(
+                            'vendor/select2/select2.min.css',
+                        ),
+            
+                        'scripts' => array(
+                            'vendor/select2/select2.min.js',
+                            'vendor/select2/app.js',
+                            'vendor/mask/jquery.mask.min.js',
+                            'vendor/mask/app.js',
+                        ),
+            
+        
+                        'conta_pagar' => $this->core_model->get_by_id('contas_pagar', array('conta_pagar_id' => $conta_pagar_id)),
+                        'fornecedores' => $this->core_model->get_all('fornecedores'),
+                );
+            
+            
+                $this->load->view('layout/header', $data);
+                $this->load->view('pagar/edit');
+                $this->load->view('layout/footer');
+            }
+        }
+    }
 
 		if(!$conta_pagar_id || !$this->core_model->get_by_id('contas_pagar', array('conta_pagar_id' => $conta_pagar_id))) {
 
