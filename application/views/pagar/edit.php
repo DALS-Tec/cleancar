efcec99aa9409ff5e48c423b2e87bcabd028c35d
Dhyon Keyllo <dhyon.kpm@hotmail.com>
2021-07-07 10:04:50 -0300

    style(code): classes

--- application/views/pagar/edit.php
+++ application/views/pagar/edit.php
@@ -20,7 +20,7 @@
                     <!-- DataTales Example -->
                     <div class="card shadow mb-4">
                         <div class="card-body">
-							<form method="POST" name="form_edit">
+							<form class="user" method="POST" name="form_edit">
 
 								<p><strong><i class="fas fa-clock"></i>&nbsp;&nbsp;Última edição:&nbsp;</strong><?php echo formata_data_banco_com_hora($conta_pagar->conta_pagar_data_alteracao) ?></p>
 
@@ -30,9 +30,9 @@
 
 									<div class="form-group row mb-4">
 
-										<div class="col-md-6">
+										<div class="col-md-4">
 											<label>Fornecedor</label>
-											<select class="form-control contas_pagar" name="conta_pagar_fornecedor_id">
+											<select class=" custom-select form-control contas_pagar" name="conta_pagar_fornecedor_id">
 												<?php foreach ($fornecedores as $fornecedor): ?>
 													<option 
 														value="<?php echo $fornecedor->fornecedor_id ?>"
@@ -49,14 +49,14 @@
 											class="form-text text-danger">','</small>'); ?>
 										</div>
 
-										<div class="col-md-2">
+										<div class="col-md-3">
 											<label>Data de vencimento</label>
 											<input type="date" class="form-control form-control-user-date" name="conta_pagar_data_vencimento" value="<?php echo $conta_pagar->conta_pagar_data_vencimento; ?>">
 											<?php echo form_error('conta_pagar_data_vencimento', '<small 
 											class="form-text text-danger">','</small>'); ?>
 										</div>
 
-										<div class="col-md-2">
+										<div class="col-md-3">
 											<label>Valor da conta</label>
 											<input type="text" class="form-control form-control-user money2" name="conta_pagar_valor" value="<?php echo $conta_pagar->conta_pagar_valor; ?>">
 											<?php echo form_error('conta_pagar_valor', '<small 
@@ -65,9 +65,9 @@
 										
 										<div class="col-md-2">
 											<label>Situação</label>
-											<select class="form-control" name="conta_pagar_status">
-												<option value="0" <?php echo ($conta_pagar->conta_pagar_status == 0) ? 'selected' : '' ?>>Paga</option>
-												<option value="1" <?php echo ($conta_pagar->conta_pagar_status == 1) ? 'selected' : '' ?>>Pendente</option>
+											<select class="form-control custom-select" name="conta_pagar_status">
+												<option value="0" <?php echo ($conta_pagar->conta_pagar_status == 0) ? 'selected' : '' ?>>Pendente</option>
+												<option value="1" <?php echo ($conta_pagar->conta_pagar_status == 1) ? 'selected' : '' ?>>Paga</option>
 											</select>
 										</div>
 									</div>
@@ -86,9 +86,9 @@
 								</fieldset>
 
 								<input type="hidden" name="conta_pagar_id" value="<?php echo $conta_pagar->conta_pagar_id; ?>" />
-								<button type="submit" class="btn btn-primary btn mr-2">
-									Salvar
-								</button>
+
+								<button type="submit" class="btn btn-primary btn mr-2" <?php echo ($conta_pagar->conta_pagar_status == 1 ? 'disabled' : '') ?>><?php echo ($conta_pagar->conta_pagar_status == 1 ? 'Conta paga' : 'Salvar') ?></button>
+
 								<a title="Voltar" href="<?php echo base_url($this->router->fetch_class()); ?>" class="btn btn-success"><i class="fas fa-arrow-left"></i>&nbsp;Voltar</a>
 
 							</form>
